d0f52ad1bc7817ac2a256bef11c5758efff87f49
Quepas <oneinchman.dev@gmail.com>
2014-08-26 00:30:10 +0200

    Small simplification: getting column by its name.

--- R Programming/Programming Assignment 1/pollutantmean.R
+++ R Programming/Programming Assignment 1/pollutantmean.R
@@ -1,19 +1,12 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   file_names <- list.files(directory, pattern="\\.csv$")
 
-  idx <-
-    if (pollutant == "sulfate") {
-      2
-    } else {
-      3
-    }
-
   data <- numeric()
   for (name in file_names) {
     path <- paste(directory, name, sep="\\")
     file <- read.csv(path)
     if (file[1,4] %in% id) {
-      data <- c(data, file[, idx])
+      data <- c(data, file[, pollutant])
     }
   }
   data <- data[!is.na(data)]
