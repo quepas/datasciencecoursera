98b1ca2713afafed275f33ca8113bc7d6c0f95d3
Quepas <oneinchman.dev@gmail.com>
2014-09-03 23:00:18 +0200

    Minor fixes.

--- R Programming/Programming Assignment 3/rankall.R
+++ R Programming/Programming Assignment 3/rankall.R
@@ -2,7 +2,8 @@ rankall <- function(outcome, num = "best") {
   ## Read outcome data
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   ## Check that state and outcome are valid
-  unique_states <- unique(data[,7])
+  unique_states <- sort(unique(data[,7]))
+  num_unique_states <- length(unique_states)
   unique_outcomes_idx <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
   if (!outcome %in% names(unique_outcomes_idx)) {
     stop("invalid outcome")
@@ -14,31 +15,30 @@ rankall <- function(outcome, num = "best") {
   ## For each state, find the hospital of the given rank
   ## Return a data frame with the hospital names and the
   ## (abbreviated) state name
-  result = data.frame(1:length(unique_states),1:length(unique_states))
+  result = data.frame(1:num_unique_states,1:num_unique_states)
   names(result) <- c("hospital", "state")
+  morality_idx <- unique_outcomes_idx[outcome]
   counter = 1
   for (state in unique_states) {
     data_from_state <- subset(data, data$State==state)
-    morality_idx <- unique_outcomes_idx[outcome]
-    data_from_state <- data_from_state[data_from_state[,morality_idx]!="Not Available",]
     data_from_state[, morality_idx] <- as.numeric(data_from_state[,morality_idx])
-    ordered_data_from_state <- data_from_state[order(data_from_state[,morality_idx], data_from_state[,2]),];
-    hospital_names <- ordered_data_from_state[,2]
+    ## Remove hospitals with NA data
+    ordered_data_from_state <- data_from_state[order(data_from_state[,morality_idx], data_from_state$Hospital.Name),];
+    hospital_names <- ordered_data_from_state$Hospital.Name
     num_hospital <- length(hospital_names)
     
     if (num == "best") {
       result[counter,1] = hospital_names[1]
-      result[counter,2] = state
     }
-    if (num == "worst") {
+    else if (num == "worst") {
       result[counter,1] = hospital_names[num_hospital]
-      result[counter,2] = state
     }
-    if (num > num_hospital) {
+    else if (num > num_hospital) {
       result[counter,1] = "NA"
-      result[counter,2] = state
     }
-    result[counter,1] = hospital_names[num]
+    else {
+      result[counter,1] = hospital_names[num]
+    }
     result[counter,2] = state
     counter = counter + 1
   }
